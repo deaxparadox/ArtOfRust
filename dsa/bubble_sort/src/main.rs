f687e7ff3a2a2bfa1cc08899a8db3e5ca64997c8
deaxparadox <nitish136900kushwaha@gmail.com>
2025-07-10 01:23:07 +0530

    bubble sort

---
+++ dsa/bubble_sort/src/main.rs
@@ -0,0 +1,23 @@
+
+fn generic_sort<T: Ord>(arr: &mut [T]) {
+    let len = arr.len();
+    for i in 0..len {
+        for j in 0..len - 1 {
+            if arr[i] > arr[j + 1] {
+                arr.swap(j, j + 1);
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut numbers = vec![3, 5, 1, 4, 2];
+    generic_sort(&mut numbers);
+    print!("{:?}", numbers);
+
+    println!("");
+
+    let mut words = vec!["hello", "how", "azmuth"];
+    generic_sort(&mut words);
+    print!("{:?}", words);
+}
